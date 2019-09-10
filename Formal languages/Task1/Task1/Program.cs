d0d7d21e800f91f9285590cb688d3e7f4c257bf1
Katerina <katerina.kamkova@mail.ru>
2019-09-10 23:18:46 +0300

    Added first task

---
+++ Formal languages/Task1/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      while (true)
+      {
+        var number = 0;
+        var currentSum = 0;
+        var i = 1;
+        var j = 0;
+
+        Console.Write("Enter the number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        while (number > currentSum)
+        {
+          ++j;
+          currentSum += j;
+        }
+
+        if (currentSum == number)
+        {
+          Console.WriteLine("i = 1, j = " + j);
+        }
+        else
+        {
+          while (number < currentSum)
+          {
+            currentSum--;
+            ++i;
+            --j;
+          }
+          Console.WriteLine("i = " + i + ", j = " + j);
+        }
+      }
+    }
+  }
+}
